02b893d74046956cff82572514ba38c3088c2845
Serentty <noname422@gmail.com>
2020-12-09 03:30:09 -0500

    Remove more testing code

--- src/arch/x86_64/unique/boot_entry_point.rs
+++ src/arch/x86_64/unique/boot_entry_point.rs
@@ -6,13 +6,11 @@ pub extern "C" fn rust_start(multiboot_ptr: u32) -> ! {
     let boot_information = unsafe { multiboot2::load(multiboot_ptr as usize) };
     if let Some(framebuffer) = boot_information.framebuffer_tag() {
         if framebuffer.buffer_type != multiboot2::FramebufferType::Text {
-            super::serial::COM1.lock().send(0x41);
             unsafe {
                 vga_graphic_console::BUFFER_BASE = transmute(framebuffer.address);
             }
         }
     }
-    super::serial::COM1.lock().send(0x42);
     crate::kmain(boot_information);
 }
 

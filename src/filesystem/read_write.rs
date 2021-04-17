9a411d4a17c6d9bd5d313900be171ccc5ca28a16
Serentty <noname422@gmail.com>
2021-04-16 21:33:46 -0400

    Fix build

--- src/filesystem/read_write.rs
+++ src/filesystem/read_write.rs
@@ -2,6 +2,11 @@ use lazy_static::lazy_static;
 
 use super::{Node, Error, InterfaceId, InterfaceTable};
 
+lazy_static! {
+    pub static ref IID_READABLE: InterfaceId = InterfaceId::allocate();
+    pub static ref IID_WRITABLE: InterfaceId = InterfaceId::allocate();
+}
+
 pub struct Readable {
     pub read: fn(&Node) -> Error
 }
